f2e4c6d6d6db486fd0a100eabebd991b4026dce1
azykdev <azyk9114@gmail.com>
2024-02-02 15:46:53 +0500

    #2 - Google fonts dan 'Poppins' fonti qo'shilda, TaliwindCss Texnalogiyasi qo'shildi.

--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -1,15 +1,15 @@
 <template>
   <div>
-    <h1>Admin</h1>
+    <h1 class="text-3xl font-bold underline text-red-500 bg-gray-900   text-center p-10">
+      Hello world!
+    </h1>
   </div>
 </template>
 
 <script>
-  export default {
-    
-  }
+export default {
+
+}
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
